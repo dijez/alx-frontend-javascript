c7d258fd52bc5696641e38dc52741d4a001d5751
dijez <khadijaahmad2k21@gmail.com>
2025-11-06 15:24:04 +0100

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,41 @@ interface Teacher {
   location: string;
   [key: string]: any;
 }
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3)
+
+
+interface Director extends Teacher{
+    // firstName : string,
+    // lastName : string,
+    // fullTimeEmployee : boolean,
+    // location : string,
+    numberOfReports: number
+}
+
+const director : Director = {
+    firstName : "jane",
+    lastName : "rose",
+    fullTimeEmployee : true,
+    location : "eygpt",
+    numberOfReports : 3,
+}
+
+console.log(director);
+
+
+interface printTeacherFunction{
+    (firstName : string, lastName : string) :string
+}
+
+const printTeacher : printTeacherFunction =(firstName , lastName) =>{
+    return `${firstName.charAt(0)}. ${lastName}`
+};
+
+console.log(printTeacher("John", "Doe"))
