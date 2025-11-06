534e64cfd59aef2e9ecef8b0408906df02ed2395
dijez <khadijaahmad2k21@gmail.com>
2025-11-06 15:21:54 +0100

    uosate

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,47 +1,8 @@
 interface Teacher {
-    readonly firstName : string;
-    readonly lastName : string;
-    fullTimeEmployee : boolean;
-    location : string;
-    yearsOfExperience? : number; 
-    [key : string] : any
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
-
-const teacher3: Teacher = {
-  firstName: 'John',
-  lastName: 'Doe',
-  fullTimeEmployee: false,
-  location: 'London',
-  contract: false,
-};
-console.log(teacher3)
-
-
-// interface Director extends Teacher{
-//     // firstName : string,
-//     // lastName : string,
-//     // fullTimeEmployee : boolean,
-//     // location : string,
-//     numberOfReports: number
-// }
-
-// const director : Director = {
-//     firstName : "jane",
-//     lastName : "rose",
-//     fullTimeEmployee : true,
-//     location : "eygpt",
-//     numberOfReports : 3,
-// }
-
-// console.log(director);
-
-
-// interface printTeacherFunction{
-//     (firstName : string, lastName : string) :string
-// }
-
-// const printTeacher : printTeacherFunction =(firstName , lastName) =>{
-//     return `${firstName.charAt(0)}. ${lastName}`
-// };
-
-// console.log(printTeacher("John", "Doe"))
