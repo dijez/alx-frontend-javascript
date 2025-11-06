1b9b2cd1b27fabcd1d3cc3fb4cec65e03a1c2003
dijez <khadijaahmad2k21@gmail.com>
2025-11-06 15:30:05 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,12 +35,11 @@ const director : Director = {
 console.log(director);
 
 
-interface printTeacherFunction{
-    (firstName : string, lastName : string) :string
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
 
-const printTeacher : printTeacherFunction =(firstName , lastName) =>{
-    return `${firstName.charAt(0)}. ${lastName}`
-};
-
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
 console.log(printTeacher("John", "Doe"))
