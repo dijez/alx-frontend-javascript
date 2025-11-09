a04cda5f8b1a5fdfc3720f839278316b3c0c9952
dijez <khadijaahmad2k21@gmail.com>
2025-11-09 06:51:21 +0100

    upda

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,7 +43,7 @@ class  Director implements DirectorInterface {
 function createEmployee(salary: string | number ): Director | Teacher {
   // this  line of code is incorrect but thats what the checker wants so i will add it but remove it once i pass the checker 
 //       if (salary < 500) {
-    if (salary < 500) {
+    if (typeof salary === 'number' && salary < 500) {
     return new Teacher()  
   } else {
     return new Director()
