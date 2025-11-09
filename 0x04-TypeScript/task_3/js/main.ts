497202f3af4a79c941e697d4b18409ec3a408943
dijez <khadijaahmad2k21@gmail.com>
2025-11-09 19:52:00 +0100

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -17,5 +17,5 @@ const newRowID : RowID = CRUD.insertRow(row)
 
 const updatedRow: RowElement = { ...row, age: 23 };
 
-CRUD.updateRow(newRowID,updatedRow)
+CRUD.updateRow(newRowID, updatedRow)
 CRUD.deleteRow(newRowID);
